76c9feb102d29b4b0bf1624531e39628016acc71
Stella Cannefax <stella@unity3d.com>
2018-04-24 22:42:59 -0700

    add threshold equality jobs

--- Assets/Scripts/Threshold/ThresholdSubtractionJobs.cs
+++ Assets/Scripts/Threshold/ThresholdSubtractionJobs.cs
@@ -18,6 +18,19 @@ namespace BurstImageProcessing.Threshold
     }
 
     [ComputeJobOptimization]
+    public struct EqualThresholdSubtractValueJob : IJobParallelFor
+    {
+        public NativeSlice<byte> data;
+        public byte threshold;
+        public byte value;
+
+        public void Execute(int i)
+        {
+            data[i] = (byte)math.select(data[i], data[i] - value, data[i] == threshold);
+        }
+    }
+
+    [ComputeJobOptimization]
     public struct UnderThresholdSubtractValueJob : IJobParallelFor
     {
         public NativeSlice<byte> data;
