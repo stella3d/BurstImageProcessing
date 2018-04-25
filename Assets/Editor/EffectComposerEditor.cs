b82fc2a1834bf2384937c2dcbf153b4bcf0d0ff3
Stella Cannefax <stella@unity3d.com>
2018-04-25 08:30:38 -0700

    add early version of the effect composer

---
+++ Assets/Editor/EffectComposerEditor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace BurstImageProcessing
+{
+    [CustomEditor(typeof(EffectComposer))]
+    public class ComposedEffectEditor : Editor
+    {
+        SerializedProperty redOperator;
+        SerializedProperty redComparator;
+        SerializedProperty redOperand;
+
+        SerializedProperty blueOperator;
+        SerializedProperty blueComparator;
+        SerializedProperty blueOperand;
+
+        EffectComposer m_Composer;
+
+        void OnEnable()
+        {
+            m_Composer = (EffectComposer)target;
+            redOperator = serializedObject.FindProperty("m_RedOperator");
+            redComparator = serializedObject.FindProperty("m_RedComparator");
+            redOperand = serializedObject.FindProperty("m_RedOperand");
+            blueOperator = serializedObject.FindProperty("m_BlueOperator");
+            blueComparator = serializedObject.FindProperty("m_BlueComparator");
+            blueOperand = serializedObject.FindProperty("m_BlueOperand");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("Per-Channel Effect Components", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(redOperator);
+            EditorGUILayout.PropertyField(redComparator);
+            EditorGUILayout.PropertyField(redOperand);
+            EditorGUILayout.Separator();
+            EditorGUILayout.PropertyField(blueOperator);
+            EditorGUILayout.PropertyField(blueComparator);
+            EditorGUILayout.PropertyField(blueOperand);
+            EditorGUILayout.Separator();
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/EffectComposer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace BurstImageProcessing
+{
+    public class EffectComposer : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(0, 255)]
+        protected byte m_AdditionValue;
+
+        [SerializeField]
+        protected Operator m_RedOperator;
+        [SerializeField]
+        protected Operand m_RedOperand;
+        [SerializeField]
+        protected Comparator m_RedComparator;
+
+        [SerializeField]
+        protected Operator m_BlueOperator;
+        [SerializeField]
+        protected Operand m_BlueOperand;
+        [SerializeField]
+        protected Comparator m_BlueComparator;
+
+        void OnEnable()
+        {
+        }
+
+        void Update()
+        {
+
+        }
+
+        void OperandHandler()
+        {
+            switch (m_RedOperator)
+            {
+                case Operator.Add:
+                    break;
+                case Operator.BitwiseComplement:
+                    break;
+                case Operator.BitwiseExclusiveOr:
+                    break;
+                case Operator.BitwiseLeftShift:
+                    break;
+                case Operator.BitwiseRightShift:
+                    break;
+            }
+        }
+    }
+}
